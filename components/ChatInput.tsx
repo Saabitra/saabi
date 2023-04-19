9dbe392dce0c3e16ad12e865209d2536ac0f5e98
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-19 06:19:18 +0000

    Chat Area

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,7 +1,11 @@
 'use client'
 
+import { db } from "@/firebase";
+import { Message } from "@/typings";
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { FormEvent, useState } from "react";
 
 type Props = {
     chatId: string;
@@ -9,12 +13,54 @@ type Props = {
 
 function ChatInput({chatId}: Props) {
     const [prompt, setPrompt] = useState('');
-  return <div className="">
-    <form className="p-5 space-x-5 flex-1">
-        <input value={prompt} onChange={e => setPrompt(e.target.value)} type="text" 
+    const {data: session} = useSession();
+
+
+    // TODO: useSwr to fetch the model
+    const model = 'text-davinci-003'
+
+
+    const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!prompt) return;
+
+        const input = prompt.trim;
+        setPrompt('');
+
+        const message: Message = {
+            text: input,
+            createdAt: serverTimestamp,
+            user: {
+                _id: session?.user?.email!,
+                name: session?.user?.name!,
+                avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name}`
+            }
+        }
+
+        await addDoc(collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'), message)
+
+        // Toast notification to say Loading!
+
+        await fetch('/api/askQuestion', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt: input, chatId, model, session
+            })
+        }).then(() => {
+            // Toast notification to say successful!
+        })
+    };
+
+  return <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
+    <form onSubmit={sendMessage} className="p-5 space-x-5 flex-1">
+        <input className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300" disabled={!session} value={prompt} onChange={e => setPrompt(e.target.value)} type="text" 
             placeholder="Enter your message here..."
         />
-        <button type="submit"><PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        <button disabled={!prompt || !session} className="bg-[#11a37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
+        type="submit"><PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
         </button>
     </form>
 
